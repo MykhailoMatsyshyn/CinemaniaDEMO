c464c9d6f6f780820db7e0414083edb14b32818c
Mykhailo <matsyshynmykhailo@gmail.com>
2024-05-17 21:02:36 +0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,62 +9,62 @@ import Logo from "../logo/Logo";
 
 export default function Footer() {
     return (
-        <div className={css.footer}>
-            <div className="container">
-                <div className={css.footer__row}>
-                    <div className={css.footer__col}>
-                        <Logo/>
-                    </div>
-
-                    <div className={css.footer__col}>
-                        <div className={css.footer__social}>
-                            <div className={css.footer__social__icon}>
-                                <a href="#">
-                                    <img src={facebook} alt=""/>
-                                </a>
-                            </div>
-                            <div className={css.footer__social__icon}>
-                                <a href="#">
-                                    <img src={instagram} alt=""/>
-                                </a>
-                            </div>
-                            <div className={css.footer__social__icon}>
-                                <a href="#">
-                                    <img src={twitter} alt=""/>
-                                </a>
-                            </div>
-                            <div className={css.footer__social__icon}>
-                                <a href="#">
-                                    <img src={youtube} alt=""/>
-                                </a>
-                            </div>
-                        </div>
-                        <div className={css.footer__rights}>
-                            © 2024 | All Rights Reserved | Developed with by <span className={css.footer__rights__hearts}>❤</span> <span>FUTUMI<span className={css.footer__rights__small}>team</span></span>
-                        </div>
-
-                    </div>
-
-                    <div className={css.footer__col}>
-                        <div className={css.footer__contact}>
-                            <div className={css.footer__contact__title}>
-                                Contact Us
-                            </div>
-                            <div className={css.footer__contact__row}>
-                                <div className={css.footer__contact__col}>
-                                    <p className={css.footer__contact__subtitle}>Phone:</p>
-                                    <p className={css.footer__contact__subtitle}>Mail:</p>
-                                </div>
-                                <div className={css.footer__contact__col}>
-                                    <p>123-456-7890</p>
-                                    <p>support@futumi.com</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+      <div className={css.footer}>
+        <div className="container">
+          <div className={css.footer__row}>
+            <div className={css.footer__col}>
+              <Logo />
             </div>
+
+            <div className={css.footer__col}>
+              <div className={css.footer__social}>
+                <div className={css.footer__social__icon}>
+                  <a href="#">
+                    <img src={facebook} alt="" />
+                  </a>
+                </div>
+                <div className={css.footer__social__icon}>
+                  <a href="#">
+                    <img src={instagram} alt="" />
+                  </a>
+                </div>
+                <div className={css.footer__social__icon}>
+                  <a href="#">
+                    <img src={twitter} alt="" />
+                  </a>
+                </div>
+                <div className={css.footer__social__icon}>
+                  <a href="#">
+                    <img src={youtube} alt="" />
+                  </a>
+                </div>
+              </div>
+              <div className={css.footer__rights}>
+                © 2024 | All Rights Reserved | Developed with{" "}
+                <span className={css.footer__rights__hearts}>❤</span> by{" "}
+                <span>
+                  FUTUMI<span className={css.footer__rights__small}>team</span>
+                </span>
+              </div>
+            </div>
+
+            <div className={css.footer__col}>
+              <div className={css.footer__contact}>
+                <div className={css.footer__contact__title}>Contact Us</div>
+                <div className={css.footer__contact__row}>
+                  <div className={css.footer__contact__col}>
+                    <p className={css.footer__contact__subtitle}>Phone:</p>
+                    <p className={css.footer__contact__subtitle}>Mail:</p>
+                  </div>
+                  <div className={css.footer__contact__col}>
+                    <p>123-456-7890</p>
+                    <p>support@futumi.com</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 }
