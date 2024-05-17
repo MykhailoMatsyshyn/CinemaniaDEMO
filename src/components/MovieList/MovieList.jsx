a11461cdd56268e2d8036c97daba3c9b2cb57121
Mykhailo <matsyshynmykhailo@gmail.com>
2024-05-17 20:43:29 +0300

    fix MovieList style

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,20 @@
 import { Link, useLocation } from "react-router-dom";
 import css from "./MovieList.module.scss";
+import { genres } from "../../constants/genres";
 
 /*======================================================================*/
 
 export default function MovieList({ movies, info }) {
   const location = useLocation();
 
+  // Функція для отримання назв жанрів за їх номерами
+  const getGenreNames = (genreIds) => {
+    const genreNames = genreIds.map((id) => genres[id] || "Unknown");
+    if (genreNames.length > 2) {
+      return `${genreNames.slice(0, 2).join(", ")}...`;
+    }
+    return genreNames.join(", ");
+  };
   // const defaultImg = "../../images/keep-calm-poster-not-found-1.png";
 
   console.log(movies);
@@ -39,28 +48,26 @@ export default function MovieList({ movies, info }) {
                 height={400}
               />
               {info !== "upcoming" && (
-                <span className={css.rate}>
-                  {vote_average.toFixed(1)}
-                </span>
-                    )}
-                    <div className={css.info}>
-                      <div className={css.info__title}>
-                        {original_title.length > 29
-                            ? `${original_title.substring(0, 29)}...`
-                            : original_title}
-                      </div>
-                      <div className="poster-list__info"></div>
-                      {info === "upcoming" && <p>{release_date}</p>}
-                      {info === "catalog" && (
-                          <div className={css.info__subtitle}>
-                            {genre_ids.join(", ")} | {release_date.substring(0, 4)}
-                          </div>
-                      )}
-                    </div>
-                  </Link>
+                <span className={css.rate}>{vote_average.toFixed(1)}</span>
+              )}
+              <div className={css.info}>
+                <div className={css.info__title}>
+                  {original_title.length > 29
+                    ? `${original_title.substring(0, 25)}...`
+                    : original_title}
                 </div>
-            )
-        )}
-      </div>
+                <div className="poster-list__info"></div>
+                {info === "upcoming" && <p>{release_date}</p>}
+                {info === "catalog" && (
+                  <div className={css.info__subtitle}>
+                    {getGenreNames(genre_ids)} | {release_date.substring(0, 4)}
+                  </div>
+                )}
+              </div>
+            </Link>
+          </div>
+        )
+      )}
+    </div>
   );
 }
